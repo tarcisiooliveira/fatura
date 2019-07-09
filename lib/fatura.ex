938301095a91f1a2049cf8142f2f27df52afc984
Tarcisio Oliveira <tarcisio@ymail.com>
2019-07-09 17:50:34 -0300

    Gerando documentação e testes das primeiras classes de aprendizado

--- lib/fatura.ex
+++ lib/fatura.ex
@@ -1,25 +1,34 @@
 defmodule Fatura do
   @moduledoc """
-  Documentation for Fatura.
+    Este móduco executamos funcoes de fatura.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Fatura.hello()
-      :world
-
+    Retorna a frase de boas vindas para o usuário
+      ## Examples
+        iex> Fatura.hello()
+        "Bem Vindo Usuario"
   """
-  def hello do
-    "asdasd"
+  def hello() do
+    "Bem Vindo Usuario"
   end
 
+  @doc """
+    Ao receber `fatura` retorna um array de faturas
+      ## Examples
+        iex> Fatura.criar_fatura()
+        ["Telefone", "Luz", "Agua "]
+  """
   def criar_fatura() do
-    ["Telefone","Luz", "Agua"]
+    ["Telefone", "Luz", "Agua "]
   end
 
+  @doc """
+    Ao receber uma lista de faturas, entrega ordenada.
+      ## Examples
+        iex> Fatura.ordenar_fatura(["Telefone", "Luz", "Agua"])
+        ["Agua", "Luz", "Telefone"]
+  """
   def ordenar_fatura(faturas) do
     Enum.sort(faturas)
   end
