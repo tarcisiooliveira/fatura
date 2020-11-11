d172e846d1e324bb196832e9c2c1ccf7687cf117
Tarcisio Oliveira <tarcisio@ymail.com>
2020-11-11 14:13:13 -0300

    correçoes, refatoraçao e notas de funcoes

--- lib/fatura.ex
+++ lib/fatura.ex
@@ -13,18 +13,18 @@ defmodule Fatura do
     "Bem Vindo Usuario"
   end
 
-  @doc """
-    Ao receber `fatura` retorna um array de faturas
-      ## Examples
-        iex> Fatura.criar_fatura(["Telefone", "Luz", "Agua"])
-        ["Telefone", "Luz", "Agua"]
+  # @doc """
+  #   Ao receber `fatura` retorna um array de faturas
+  #     ## Examples
+  #       iex> Fatura.criar_fatura(["Telefone", "Luz", "Agua"])
+  #       ["Telefone", "Luz", "Agua"]
 
-        iex> Fatura.criar_fatura("bola")
-        "bola"  
-  """
-  def criar_fatura(fatura) do
-    fatura
-  end
+  #       iex> Fatura.criar_fatura("bola")
+  #       "bola"
+  # """
+  # def criar_fatura(fatura) do
+  #   fatura
+  # end
 
   @doc """
     A função `criar_faturas` recebe duas listas de faturas e vencimentos e retorna uma estrutura de `Fatura.Conta`
@@ -48,7 +48,7 @@ defmodule Fatura do
   @doc """
     Ao receber uma lista de faturas, entrega ordenada.
         ## Examples
-          iex> listbill = Fatura.criar_faturas(["Telefone", "Luz", "Agua"], [5,9])      
+          iex> listbill = Fatura.criar_faturas(["Telefone", "Luz", "Agua"], [5,9])
           iex> Fatura.sort_bill(listbill)
           [
             %Fatura.Conta{fatura: "Agua", vencimento: 5},
@@ -75,27 +75,27 @@ defmodule Fatura do
 
   """
   def fatura_existe?(faturas, fatura) do
-    # question symbol means how will return a boolena value
-    Enum.member?(faturas, fatura)
+      Enum.member?(faturas, fatura)
   end
 
+  # @spec save(list(),charlist()) :: :ok
   @doc """
     Exemplo de criação de arquivo com uma lista dentro
       ## Examples
-        iex> arquivo=Fatura.criar_fatura(["Telefone", "Luz", "Agua"])
-        iex> Fatura.save("bill_to_pay",arquivo)
+        iex> faturas=Fatura.criar_faturas(["Telefone", "Luz", "Agua"], [5,9])
+        iex> Fatura.save(faturas,"bill_to_pay")
         :ok
   """
-  def save(filename, faturas) do
+  def save(faturas,filename) do
     binary = :erlang.term_to_binary(faturas)
     File.write(filename, binary)
   end
 
   @doc """
     Remove a `fatura` passada no `index`
-    Remove `bill` past in `index`
+    Remove `bill` past in `index`sasd
       ## Exemples
-        iex>Enum.split([1,2,3,4,5],1)
+        iex>Fatura.pay_bill([1,2,3,4,5],1)
         {[1], [2, 3, 4, 5]}
   """
   def pay_bill(bill,index) do
@@ -105,36 +105,42 @@ defmodule Fatura do
   @doc """
     Quando encontrar uma fatura
       ## Examples
-          iex>  arquivo=Fatura.criar_fatura(["Telefone", "Luz", "Agua"])
-          iex>  Fatura.save("bill_to_pay",arquivo)
+          iex>  arquivo=Fatura.criar_faturas(["Telefone", "Luz", "Agua"], [5,9])
+          iex>  Fatura.save(arquivo,"bill_to_pay")
           :ok
           iex>  Fatura.loadfile("bill_to_pay")
-          ["Telefone", "Luz", "Agua"]
-          
-          iex>  arquivo=Fatura.criar_fatura(["Telefone", "Luz", "Agua"])
-          iex>  Fatura.save("bill_to_pay",arquivo)
-          :ok
-          iex>  Fatura.loadfile("filenotexist")
-          "File no found"
+          [
+            %Fatura.Conta{fatura: "Telefone", vencimento: 5},
+            %Fatura.Conta{fatura: "Luz", vencimento: 5},
+            %Fatura.Conta{fatura: "Agua", vencimento: 5},
+            %Fatura.Conta{fatura: "Telefone", vencimento: 9},
+            %Fatura.Conta{fatura: "Luz", vencimento: 9},
+            %Fatura.Conta{fatura: "Agua", vencimento: 9}
+          ]
+
+
   """
   def loadfile(filename) do
     {status, binary} = File.read(filename)
     case status do
-      :ok -> printbinaryfile(binary)
+      :ok -> print_binary_file(binary)
       :error -> "File no found"
     end
   end
 
+  def faturas_a_pagar(faturas, quantidade) do
+    Enum.split(faturas, quantidade)
+  end
   @doc """
       Receive `file_name` and print all data.
         ## Exemples
-         
-          
+
+
   """
-  def printbinaryfile(file) do
+  def print_binary_file(file) do
     :erlang.binary_to_term(file)
   end
-  
+
 
   @doc """
     Access list bills by index
@@ -148,4 +154,12 @@ defmodule Fatura do
     Enum.with_index(bills)
     |> Enum.at(index)
   end
+
+  def pagar_fatura(faturas, vencimento, quantidade, nome_arquivo) do
+    criar_faturas(faturas, vencimento)
+    |> sort_bill()
+    |> pay_bill(quantidade)
+    |> save(nome_arquivo)
+  end
+
 end
